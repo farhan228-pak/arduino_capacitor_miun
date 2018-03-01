6261ee9627cd02cd71cccc12f5962a22b7b217d8
PERSONAL\farala <farhan.alam@seps.se>
2018-03-01 12:59:57 +0100

    Cell voltage

--- arduino_capacitor.ino
+++ arduino_capacitor.ino
@@ -7,11 +7,12 @@ float voltage,cell1,cell2,cell3,cell4;
 float node0,node1,node2,node3,node4,node5,node6,node7,node8;
 long previousMillis = 0;
 long interval = 10000;
-float refrence=5;
+float refrence=4.9;
+int pro_begin=0,slected=0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(256000);
   pinMode(sp11, OUTPUT);
   pinMode(sp12, OUTPUT);
   pinMode(sp21, OUTPUT);
@@ -38,48 +39,48 @@ digitalWrite(sp22, LOW);
 void loop() {
   // put your main code here, to run repeatedly:
 unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis > interval) {
- 
+  if(currentMillis - previousMillis > interval || pro_begin==0) {
+    pro_begin=1;
     previousMillis = currentMillis;   
     clr_screen();
-Serial.println("enter number to select one of the 4 options");
-
-Serial.println("Case ""0"" All cell disconnected");
-Serial.println("case ""1"" Two in prallel then combined in prallel -->All in prallel");
-Serial.println("case ""2"" Two in series then combined in prallel");
-Serial.println("case ""3"" Two in series then combined in Series -->ALL in series");
-Serial.println("case ""4"" ALL in series for charging");
-Serial.println();
+//Serial.println("enter number to select one of the 4 options");
+//
+//Serial.println("Case ""0"" All cell disconnected");
+//Serial.println("case ""1"" Two in prallel then combined in prallel -->All in prallel");
+//Serial.println("case ""2"" Two in series then combined in prallel");
+//Serial.println("case ""3"" Two in series then combined in Series -->ALL in series");
+//Serial.println("case ""4"" ALL in series for charging");
+//Serial.println();
 
 command = (int)Serial.read();
 //Serial.write(command);
-Serial.print("A0=");
+//Serial.print("A0=");
 node0=analogRead(A0)*(refrence/1023)*11;
-Serial.println(node0);
-Serial.print("A1=");
+//Serial.println(node0);
+//Serial.print("A1=");
 node1=analogRead(A1)*(refrence/1023)*11;
-Serial.println(node1);
-Serial.print("A2=");
+//Serial.println(node1);
+//Serial.print("A2=");
 node2=analogRead(A2)*(refrence/1023)*11;
-Serial.println(node2);
-Serial.print("A3=");
+//Serial.println(node2);
+//Serial.print("A3=");
 node3=analogRead(A3)*(refrence/1023)*11;
-Serial.println(node3);
-Serial.print("A4=");
+//Serial.println(node3);
+//Serial.print("A4=");
 node4=analogRead(A4)*(refrence/1023)*11;
-Serial.println(node4);
-Serial.print("A5=");
+//Serial.println(node4);
+//Serial.print("A5=");
 node5=analogRead(A5)*(refrence/1023)*11;
-Serial.println(node5);
-Serial.print("A6=");
+//Serial.println(node5);
+//Serial.print("A6=");
 node6=analogRead(A6)*(refrence/1023)*11;
-Serial.println(node6);
-Serial.print("A7=");
+//Serial.println(node6);
+//Serial.print("A7=");
 node7=analogRead(A7)*(refrence/1023)*11;
-Serial.println(node7);
-Serial.println("");
-Serial.println("");
-Serial.println("");
+//Serial.println(node7);
+//Serial.println("");
+//Serial.println("");
+//Serial.println("");
 cell1=node7-node6;
 Serial.print("Cell1=");
 Serial.println(cell1);
@@ -98,94 +99,107 @@ Serial.println(cell3);
 cell4=node1-node0;
 Serial.print("Cell4=");
 Serial.println(cell4);
+Serial.print("Final output=");
+Serial.println(node7-node0);
+selected_option();
   }
-  if(command>-1)
-  {
-command=command-48;
-//Serial.println(command);
-  }
-switch(command)
-{
+//  if(command>-1)
+//  {
+//command=command-48;
+////Serial.println(command);
+//  }
+//switch(command)
+//{
   
-case 0: //all off no outputt
-      
+//case 0: //all off no outputt
+ /*     
 digitalWrite(sp11, LOW);
 digitalWrite(sp12, LOW);
 digitalWrite(sp21, LOW);
 digitalWrite(sp22, LOW);
-digitalWrite(13, HIGH);
+digitalWrite(13, HIGH);*/
 Serial.println("All cells disconnected no output ");
- break;
+slected=0;
+pro_begin=0;
+// break;
 
-case 1:// Two in prallel  combined in prallel
+//case 1:// Two in prallel  combined in prallel
 Serial.println("case 1 = Two in prallel  combined in prallel");
-digitalWrite(sp11, LOW);
-digitalWrite(sp12, LOW);
-digitalWrite(sp21, LOW);
-digitalWrite(sp22, LOW);
-
-delay(3);
+//digitalWrite(sp11, LOW);
+//digitalWrite(sp12, LOW);
+//digitalWrite(sp21, LOW);
+//digitalWrite(sp22, LOW);
+//
+//delay(3);
 digitalWrite(sp12, LOW);
 digitalWrite(sp11, HIGH);
 digitalWrite(sp21, LOW);
 digitalWrite(sp22, HIGH);
-delay(1);
- break;
-case 2:// Two in series then cobined in prallel
+//delay(1);
+slected=1;
+pro_begin=0;
+// break;
+//case 2:// Two in series then cobined in prallel
 
 Serial.println("Case 2 = Two in series then cobined in prallel");
-digitalWrite(sp11, LOW);
-digitalWrite(sp12, LOW);
-digitalWrite(sp21, LOW);
-digitalWrite(sp22, LOW);
-
-delay(3);
+//digitalWrite(sp11, LOW);
+//digitalWrite(sp12, LOW);
+//digitalWrite(sp21, LOW);
+//digitalWrite(sp22, LOW);
+//
+//delay(3);
 
 digitalWrite(sp11, LOW);
 digitalWrite(sp12, HIGH);
 digitalWrite(sp21, LOW);
 digitalWrite(sp22, HIGH);
-break;
+slected=2;
+pro_begin=0;
+//break;
 delay(1);
-case 3://Two in series combined in series
+
+//case 3://Two in series combined in series
 Serial.println(" case 3 = Two in series then combined in Series");  
-digitalWrite(sp11, LOW);
-digitalWrite(sp12, LOW);
-digitalWrite(sp21, LOW);
-digitalWrite(sp22, LOW);
+//digitalWrite(sp11, LOW);
+//digitalWrite(sp12, LOW);
+//digitalWrite(sp21, LOW);
+//digitalWrite(sp22, LOW);
 
-delay(3);
-digitalWrite(sp11, LOW);
-digitalWrite(sp12, HIGH);
-digitalWrite(sp22, LOW);
-digitalWrite(sp21, HIGH);
-
-delay(1);
-break;
-
-case 4:
-Serial.println(" case 4 = All in series for charging");  
-digitalWrite(sp11, LOW);
-digitalWrite(sp12, LOW);
-digitalWrite(sp21, LOW);
-digitalWrite(sp22, LOW);
-
-delay(3);
+//delay(3);
 digitalWrite(sp11, LOW);
 digitalWrite(sp12, HIGH);
 digitalWrite(sp22, LOW);
 digitalWrite(sp21, HIGH);
 
-delay(1);
+//delay(1);
+slected=3;
+pro_begin=0;
+//break;
 
-break;
+//case 4:
+//Serial.println(" case 4 = All in series for charging");  
+//digitalWrite(sp11, LOW);
+//digitalWrite(sp12, LOW);
+//digitalWrite(sp21, LOW);
+//digitalWrite(sp22, LOW);
 
-default:
+//delay(3);
+//digitalWrite(sp11, LOW);
+//digitalWrite(sp12, HIGH);
+//digitalWrite(sp22, LOW);
+//digitalWrite(sp21, HIGH);
 
-break;
-}
+//delay(1);
+slected=4;
+pro_begin=0;
+//break;
 
-delay(500);
+//default:
+
+//break;
+//}
+
+//delay(500);
 
 
 }
@@ -193,8 +207,45 @@ delay(500);
 void clr_screen(void)
 {
    Serial.write(27);       // ESC command
-  Serial.print("[2J");    // clear screen command
+ // Serial.print("[2J");    // clear screen command
   Serial.write(27);
   Serial.print("[H");     // cursor to home command
 }
+
+void selected_option(void)
+{
+    Serial.write(27);
+  Serial.print("[J");     // clear screen from curssor point command
+  
+  if(slected==0)
+  {
+    Serial.println("ALL outputs off");
+  }
+
+
+  if(slected==1)
+  {
+    Serial.println("All in prallel ");
+  }
+
+  if(slected==2)
+  {
+  
+    Serial.println("Two in series then cobined in prallel");
+  }
+
+
+  if(slected==3)
+  {
+    Serial.println("ALL in series    ");
+  }
+
+  
+  if(slected==4)
+  {
+    Serial.println("charging");
+  }
+}
+
+
 
